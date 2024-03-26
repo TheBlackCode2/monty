99c280957758ceb395f93f8a729c05c8c2c44260
TheBlackCode2 <yassinew20w@gmail.com>
2024-03-26 14:58:55 +0000

    New update

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    FILE *fptr;
+    char myString[100];
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    fptr = fopen(argv[1], "r");
+
+    if(fptr == NULL)
+    {
+        printf("Error: Can't open file %s", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    while(fgets(myString, 100, fptr))
+        printf("%s", myString);
+  
+    fclose(fptr);
+
+    return EXIT_SUCCESS;
+}
